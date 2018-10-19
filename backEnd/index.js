e06f1fce722c7b1b581f7ad59bcd1fe88bfc94cc
JaehoLee <spectrick@gmail.com>
2018-10-19 16:32:56 +0900

    [implementaion]  backEnd에도 peer:discovery event handler 추가

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,6 +7,19 @@ const initApp = () => {
     console.log("node created");
     console.log("node is ready", node.peerInfo.id.toB58String());
     console.log(node.peerInfo.multiaddrs.toArray().map(o=>o.toString()));
+    node.on("peer:discovery", peerInfo => {
+      const idStr = peerInfo.id.toB58String();
+      console.log("Discovered: " + idStr);
+
+      node.dialProtocol(peerInfo, '/kitty', (err, conn) => {
+        if (err) {
+          //return console.log("Failed to dial:", idStr);
+          return;
+        }
+        console.log("hooray!", idStr);
+      });
+    });
+
     node.handle('/kitty',(protocol, conn) => {
       console.log("dialed!!");
     });
