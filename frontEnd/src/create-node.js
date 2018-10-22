e4826868bfed6a788f021c4fa6476223a9642687
JaehoLee <spectrick@gmail.com>
2018-10-23 02:16:50 +0900

    add a private websocket rendezvous node.

--- frontEnd/src/create-node.js
+++ frontEnd/src/create-node.js
@@ -4,7 +4,8 @@ const multiaddr = require('multiaddr');
 const createNode = new Promise((resolve, reject) => {
   PeerInfo.create((err, peerInfo) => {
     if (err) reject(err);
-    peerInfo.multiaddrs.add(multiaddr("/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star/"));
+    // peerInfo.multiaddrs.add(multiaddr("/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star/"));
+    peerInfo.multiaddrs.add(multiaddr("/ip4/64.137.231.84/tcp/9090/ws/p2p-websocket-star/"));
     const node = new Node({peerInfo});
     resolve(node);
   });
