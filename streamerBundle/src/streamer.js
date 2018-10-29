1159f06b40b776f60fa066589421c276315e953a
hhan <hhan87@gmail.com>
2018-10-29 18:41:53 +0900

    refactoring streamer code

--- streamerBundle/src/streamer.js
+++ streamerBundle/src/streamer.js
@@ -1,26 +1,21 @@
 const Janus = require('./janus.es.js').default;
-var roomId = parseInt(document.querySelector(".roomId").value, 10);
-var sfutest = null;
 var opaqueId = "videoroomtest-"+Janus.randomString(12);
 const endPoint = "http://13.209.96.83:8088/janus";
-
+const roomId = 1;
 
 let init = ()=>{
+    
     //setting janus.Js
-    Janus.init({debug :"all", callback : ()=>{
-        Janus.log("Initiating Janus....");
-        return new Promise((res, rej)=>{
-            res();
-        });
-    }})
+    (new Promise((resolve, reject)=>{
+        Janus.init({debug :"all", callback : ()=>{
+            Janus.log("Initiating Janus....");
+            resolve();
+        }});
+    }))
     .then(()=>{
         //Connect to Janus Server....
-    })
-
-
-    //setting janus.Js
-    Janus.init({debug :"all", callback : function(){
-        Janus.log("Initiating Janus....");
+        //setting janus.Js
+        let sfutest = null;
         let janus = new Janus({
             server : endPoint,
             success : x=>{
@@ -31,12 +26,21 @@ let init = ()=>{
                     success : (pluginHandle)=>{
                         sfutest = pluginHandle;
                         Janus.log("Plugin attached! (" + sfutest.getPlugin() + ", id=" + sfutest.getId() + ")");
-                        /*sfutest.send({message:{request:"create", 
-                            room: roomId, 
-                            videocodec : "H264",
-                            audiocodec : "opus",
-                            notify_joining : true
-                        }});*/
+                        sfutest.send({
+                            message: {
+                                request:"create", 
+                                room: roomId, 
+                                videocodec : "H264",
+                                audiocodec : "opus",
+                                notify_joining : true
+                            },
+                            success : (data)=>{
+                                displayMessage("You are allocated to Room "+roomId);
+                            },
+                            error : e =>{
+                                Janus.error(e);
+                            }
+                        });    
                     },
                     error : (err)=>{
                         Janus.error(" error attaching plugin", error);
@@ -75,18 +79,32 @@ let init = ()=>{
                                         Janus.error("WebRTC error:", error);
                                     }
                                 });
+                        }else if(msg["videoroom"] === "event"){
+                            if(msg["unpublished"] !== undefined && msg["unpublished"] !== null) {
+                                // One of the publishers has unpublished?
+                                var unpublished = msg["unpublished"];
+                                Janus.log("Publisher left: " + unpublished);
+                                if(unpublished === 'ok') {
+                                    // That's us
+                                    sfutest.hangup();
+                                    sfutest.
+                                    return;
+                                }
+                            }
                         }
                         if(jsep !== undefined && jsep !== null){
                             Janus.debug("Receive Handling SDP as well...");
                             Janus.debug(jsep);
-                            sfutest.handleRemoteJsep({jsep: jsep, success : x =>{
-                                console.log("success handle remote");
-                            }, 
-                            error : e=>{
-                                console.error(e);
-                            }});
+                            sfutest.handleRemoteJsep({
+                                jsep: jsep, 
+                                success : x =>{
+                                    Janus.log(" ::: Success handle remote jesp ::: ");
+                                }, 
+                                error : e=>{
+                                    console.error(e);
+                                }
+                            });
                         }
-
                     },
                     onlocalstream : (stream)=>{
                         Janus.debug(" ::: Got a local stream :::");
@@ -102,12 +120,30 @@ let init = ()=>{
             destroyed : x=>{
                 console.log("destroyed");
             }
-        })    
-    }})
-    //add events
-    document.querySelector(".start-btn").addEventListener("click", ()=>[
-        sfutest.send({message:{request:"join", room: roomId, ptype:"publisher", id: parseInt(Math.random()*1000000, 10)}})
-    ]);
-    
+        });
+            
+        //add events
+        //start broadcast
+        document.querySelector(".start-btn").addEventListener("click", ()=>{
+            sfutest.send({message:{
+                request:"join", 
+                room: roomId, 
+                ptype:"publisher", 
+                id: parseInt(Math.random()*1000000, 10)}
+            });    
+            
+        }); 
+        //stop broadcast
+        document.querySelector(".stop-btn").addEventListener("click", ()=>{
+            sfutest.send({"message":
+                { "request": "unpublish" }
+            });
+            sfutest.send({"message":
+                { "request": "leave" }
+            });
+        }); 
+    });
+
+
 }
 init();
--- streamerBundle/src/viewer.js
+++ streamerBundle/src/viewer.js
@@ -1,107 +1,140 @@
 const Janus = require('./janus.es.js').default;
-var roomId = parseInt(document.querySelector(".roomId").value, 10);
-var sfutest = null;
 var opaqueId = "videoroomtest-"+Janus.randomString(12);
 const endPoint = "http://13.209.96.83:8088/janus";
 
+
 let init = ()=>{
-    Janus.init({debug :"all", callback : function(){
-        console.log("start");
-        var roomId = parseInt(document.querySelector(".roomId").value, 10);
-        console.log("init start");
-            let janus = new Janus({
-                server : endPoint,
-                success : x=>{
-                    console.log("success");
-                    janus.attach({
-                        plugin : "janus.plugin.videoroom",
-                        opaqueId : opaqueId,
-                        success : (pluginHandle)=>{
-                            sfutest = pluginHandle;
-                            Janus.log("Plugin attached! (" + sfutest.getPlugin() + ", id=" + sfutest.getId() + ")");
-                            sfutest.send({message:{request:"listparticipants", room : roomId}, success : msg =>{
-                                console.log("participants")
-                                console.log(msg);
-                                if( msg.participants[0] !== undefined
-                                    && msg.participants[0] !== null){
-                                        sfutest.videoCodec = "H264"
-                                        sfutest.send({message:{ 
-                                            "request" : "join", 
-                                            "room"  : roomId, 
-                                            "ptype" : "subscriber", 
-                                            "feed"  : msg.participants[0].id,  
-                                            "private_id" : parseInt(Math.random()*1000000, 10) }});
+    //setting janus.Js
+    (new Promise((resolve, reject)=>{
+        Janus.init({debug :"all", callback : ()=>{
+            Janus.log("Initiating Janus....");
+            resolve();
+        }});
+    }))
+    .then(()=>{
+        //Connect to Janus Server....
+        //setting janus.Js
+        let sfutest = null;
+        let janus = new Janus({
+            server : endPoint,
+            success : x=>{
+                Janus.log("Success Connect to Janus Server....");
+                janus.attach({
+                    plugin : "janus.plugin.videoroom",
+                    opaqueId : opaqueId,
+                    success : (pluginHandle)=>{
+                        sfutest = pluginHandle;
+                        Janus.log("Plugin attached! (" + sfutest.getPlugin() + ", id=" + sfutest.getId() + ")");
+                    },
+                    error : (err)=>{
+                        Janus.error(" error attaching plugin", error);
+                    },
+                    consentDialog : (on)=>{
+                        Janus.debug("Consent dialog should be " + (on ? "on" : "off") + " now");
+                    },
+                    mediaState : (medium, on)=>{
+                        Janus.log("Janus " + (on ? "started" : "stopped") + " receiving our " + medium);
+                    },
+                    webrtcState : (on)=>{
+                        Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
+                    },
+                    onmessage : (msg, jsep)=>{
+                        Janus.debug(" ::: Got a message (Subscriber) :::");
+                        console.log(msg);
+                        if(msg["videoroom"] === "attached"){
+                            let roomId = parseInt(document.querySelector(".roomId").value,10);
+                            console.log("attached");
+                            console.log(jsep);
+                            if(jsep !== undefined && jsep !== null) {
+                                Janus.debug("Handling SDP as well...");
+                                Janus.debug(jsep);
+                                // Answer and attach
+                                sfutest.createAnswer(
+                                    {
+                                        jsep: jsep,
+                                        // Add data:true here if you want to subscribe to datachannels as well
+                                        // (obviously only works if the publisher offered them in the first place)
+                                        media: { 
+                                            audioRecv: true, 
+                                            videoRecv: true, 
+                                            audioSend: false, 
+                                            videoSend: false 
+                                        },	// We want recvonly audio/video
+                                        success: function(jsep) {
+                                            Janus.debug("Got SDP!");
+                                            Janus.debug(jsep);
+                                            var body = { "request": "start", "room": roomId };
+                                            sfutest.send({"message": body, "jsep": jsep});
+                                        },
+                                        error: function(error) {
+                                            Janus.error("WebRTC error:", error);
+                                            bootbox.alert("WebRTC error... " + JSON.stringify(error));
+                                        }
+                                    });
                                 }
-                            }}
-                            
-                            );
-                            console.log(sfutest.send)
-                        },
-                        error : (err)=>{
-                            Janus.error(" error attaching plugin", error);
-                        },
-                        consentDialog : (on)=>{
-                            Janus.debug("Consent dialog should be " + (on ? "on" : "off") + " now");
-                        },
-                        mediaState : (medium, on)=>{
-                            Janus.log("Janus " + (on ? "started" : "stopped") + " receiving our " + medium);
-                        },
-                        webrtcState : (on)=>{
-                            Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
-                        },
-                        onmessage : (msg, jsep)=>{
-                            Janus.debug(" ::: Got a message (sub) :::");
-                            if(msg["videoroom"] === "joined"){
-                                Janus.debug("Joined!");
-                                console.log(msg);                                
-                                
-                            }else if(msg["videoroom"] === "attached"){
-                                console.log("attached");
-                                console.log(jsep);
-                                if(jsep !== undefined && jsep !== null) {
-                                    Janus.debug("Handling SDP as well...");
-                                    Janus.debug(jsep);
-                                    // Answer and attach
-                                    sfutest.createAnswer(
-                                        {
-                                            jsep: jsep,
-                                            // Add data:true here if you want to subscribe to datachannels as well
-                                            // (obviously only works if the publisher offered them in the first place)
-                                            media: { audioRecv: true, 
-                                                videoRecv: true, audioSend: false, videoSend: false },	// We want recvonly audio/video
-                                            success: function(jsep) {
-                                                Janus.debug("Got SDP!");
-                                                Janus.debug(jsep);
-                                                var body = { "request": "start", "room": roomId };
-                                                sfutest.send({"message": body, "jsep": jsep});
-                                            },
-                                            error: function(error) {
-                                                Janus.error("WebRTC error:", error);
-                                                bootbox.alert("WebRTC error... " + JSON.stringify(error));
-                                            }
-                                        });
-                                    }
-                            }
-                        },
-                        onlocalstream : (stream)=>{
-                            Janus.debug(" ::: Got a local stream :::");
-                            
-                        },
-                        onremotestream : (stream)=>{
-                            Janus.debug(" ::: Got a remote stream :::");
-                            Janus.attachMediaStream(document.querySelector(".video"),stream);
-                        },
-                    })
-                },
-                error : x=>{
-                    console.error(x);
-                },
-                destroyed : x=>{
-                    console.log("destroyed");
+                        }
+                    },
+                    onlocalstream : (stream)=>{
+                        Janus.debug(" ::: Got a local stream :::");
+                        
+                    },
+                    onremotestream : (stream)=>{
+                        Janus.debug(" ::: Got a remote stream :::");
+                        Janus.attachMediaStream(document.querySelector(".video"),stream);
+                    }
+                })
+            },
+            error : x=>{
+                //Janus.error(x);
+                console.error(x);
+            },
+            destroyed : x=>{
+                console.log("destroyed");
+            }
+        });
+            
+        //add events
+        //start broadcast
+        document.querySelector(".join-btn").addEventListener("click", ()=>{
+            let roomId = parseInt(document.querySelector(".roomId").value,10);
+            sfutest.send({message:{
+                request:"listparticipants", room : roomId}, 
+                success : msg => {
+                    Janus.log("participant msg");
+                    Janus.log(msg);
+                    if(msg["videoroom"] === "event"){
+                        displayMessage(msg)
+                    }else if( msg.participants[0] !== undefined
+                        && msg.participants[0] !== null){
+                            sfutest.videoCodec = "H264"
+                            sfutest.send({
+                                message : { 
+                                    "request" : "join", 
+                                    "room"  : roomId, 
+                                    "ptype" : "subscriber", 
+                                    "feed"  : msg.participants[0].id,  
+                                    "private_id" : parseInt(Math.random()*1000000, 10) 
+                                }
+                            });
+                    }
                 }
-            })        
-        }
+            }); 
+            //stop broadcast
+            document.querySelector(".leave-btn")
+            .addEventListener("click", ()=>{
+                sfutest.send({"message":
+                    { "request": "unpublish" }
+                });
+                sfutest.send({"message":
+                    { "request": "leave" }
+                });
+            }); 
+        });
     });
 }
-document.querySelector(".join-btn").addEventListener("click", joinInit);
+const displayMessage = (msg)=>{
+    document.querySelector(".message").innerText = JSON.stringify(msg);
+}
+
+
 init();
