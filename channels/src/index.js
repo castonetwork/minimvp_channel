0c70039a0ab00e4211107a6fef5c48fffcda6c0c
hhan <hhan87@gmail.com>
2018-11-07 12:33:35 +0900

    fix channel sendController error

--- channels/src/index.js
+++ channels/src/index.js
@@ -1,5 +1,6 @@
-const pull = require('pull-stream');
-const Pushable = require('pull-stream');
+const pull = require("pull-stream");
+const Pushable = require("pull-pushable");
+const { tap } = require("pull-tap");
 let sendController = Pushable();
 window.pull = pull;
 const createNode = require("./create-node");
@@ -10,54 +11,48 @@ const updateChannelInfo = info => {
   if (infoDiv) {
     infoDiv.textContent = JSON.stringify(info);
   } else {
-    const dom = document.createElement('div');
+    const dom = document.createElement("div");
     dom.textContent = JSON.stringify(info);
-    dom.setAttribute('id', info.id);
+    dom.setAttribute("id", info.id);
     document.body.appendChild(dom);
   }
 };
 
 const processEvents = event => {
   const events = {};
-  events[type] && events[type](event);
+  events[event.type] && events[event.type](event);
 };
 
 const initApp = () => {
   console.log("init app");
-  createNode
-    .then(node => {
-      window.currentNode = node;
-      node.on("peer:discovery", peerInfo => {
-        const idStr = peerInfo.id.toB58String();
-        console.log("Discovered: " + idStr);
-        updateChannelInfo({id: idStr});
-        node.dialProtocol(peerInfo, '/controller', (err, conn) => {
-          if (err) {
-            // console.error("Failed to dial:", err);
-            return;
-          }
-          pull(
-            sendController,
-            pull.map(JSON.stringify),
-            conn,
-            processEvents,
-            pull.log()
-          );
-          sendController.push({
-            type: "channelRegister",
-            idStr: idStr
-          });
-        });
-      });
-      node.on("peer:connect", peerInfo => {
-        console.log("connected peerInfo: ", peerInfo.id.toB58String());
-        console.log(peerInfo);
-      });
-      node.start(err => {
-        if (err) throw err;
-        console.log("node is ready", node.peerInfo.id.toB58String());
-        console.log(node.peerInfo.multiaddrs.toArray().map(o => o.toString()));
+  createNode.then(node => {
+    window.currentNode = node;
+    node.on("peer:discovery", peerInfo => {
+      const idStr = peerInfo.id.toB58String();
+      console.log("Discovered: " + idStr);
+      updateChannelInfo({ id: idStr });
+      node.dialProtocol(peerInfo, "/controller", (err, conn) => {
+        if (err) {
+          // console.error("Failed to dial:", err);
+          return;
+        }
+        pull(sendController, tap(console.log), pull.map(JSON.stringify), conn);
+        pull(conn, tap(console.log), processEvents, pull.log());
+        // sendController.push({
+        //   type: "channelRegister",
+        //   idStr: idStr
+        // });
       });
     });
+    node.on("peer:connect", peerInfo => {
+      console.log("connected peerInfo: ", peerInfo.id.toB58String());
+      console.log(peerInfo);
+    });
+    node.start(err => {
+      if (err) throw err;
+      console.log("node is ready", node.peerInfo.id.toB58String());
+      console.log(node.peerInfo.multiaddrs.toArray().map(o => o.toString()));
+    });
+  });
 };
 document.addEventListener("DOMContentLoaded", initApp);
