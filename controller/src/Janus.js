8e4668fb99440a7f55500adb5ba63b25a7ebf766
JaehoLee <spectrick@gmail.com>
2018-11-19 11:51:26 +0900

    fix: #2 pushable만 추가하여 상태를 모니터하도록 변경

--- controller/src/Janus.js
+++ controller/src/Janus.js
@@ -1,3 +1,4 @@
+const Pushable = require("pull-pushable");
 const spawn = require("child_process").spawn;
 const janusProcessname =  __dirname + "/bin/janus_standalone/run.sh";
 //const janusCfg = "/opt/janus/etc/janus/janus.cfg";
@@ -7,85 +8,92 @@ class Janus {
   constructor() {
     //super();
     this.webSocketsThreadStarted = "WebSockets thread started";
-    return this._runJanus();
+    this.handler = Pushable();
+    this._runJanus();
   }
   _runJanus() {
-    return new Promise((resolve, reject)=>{
-      console.log("run janus");
+    console.log("run janus");
 
-      /*if (this._minRtpPort && this._maxRtpPort) {
-        janusArgs.push(
-          util.format(`--rtp-port-range=${this._minRtpPort}-${this._maxRtpPort}`)
+    /*if (this._minRtpPort && this._maxRtpPort) {
+      janusArgs.push(
+        util.format(`--rtp-port-range=${this._minRtpPort}-${this._maxRtpPort}`)
+      );
+    }
+
+    if (this._stunUrl) {
+      janusArgs.push(util.format(`--stun-server=${this._stunUrl}`));
+    }
+
+    if (this._publicIp) {
+      janusArgs.push(util.format(`--nat-1-1=${this._publicIp}`));
+    }
+
+    if (this._noMediaTimer) {
+      janusArgs.push(util.format(`--no-media-timer=${this._noMediaTimer}`));
+    }*/
+
+    let spawnOptions = {
+      detached: false,
+      /*
+       * fd 0 (stdin)   : Just ignore it.
+       * fd 1 (stdout)  : Pipe it for 3rd libraries in the worker.
+       *                  that log their own stuff.
+       * fd 2 (stderr)  : Same as stdout.
+       */
+      stdio: ["ignore", "pipe", "pipe"]
+      //    timeout: 3000,
+      //    killSignal: 'SIGKILL'
+    };
+
+    this._child = spawn(janusProcessname, janusArgs, spawnOptions);
+    console.log("janus: pid=" + this._child.pid);
+    this._child.stdout.on("data", buffer => {
+      if (buffer.slice(-1) == "\n") {
+        console.log(buffer.subarray(0, buffer.length - 1).toString());
+      } else {
+        console.log(buffer.toString());
+      }
+      if (buffer.toString().split("\n").some(o=>o===this.webSocketsThreadStarted)) {
+        this.handler.push({
+          type: "ready",
+          status: true
+        })
+      }
+    });
+
+    this._child.stderr.on("data", buffer => {
+      if (buffer.slice(-1) == "\n") {
+        console.error(
+          "janus: stderr=" + buffer.subarray(0, buffer.length - 1).toString()
         );
+      } else {
+        console.error("janus: stderr=" + buffer.toString());
       }
-
-      if (this._stunUrl) {
-        janusArgs.push(util.format(`--stun-server=${this._stunUrl}`));
-      }
-
-      if (this._publicIp) {
-        janusArgs.push(util.format(`--nat-1-1=${this._publicIp}`));
-      }
-
-      if (this._noMediaTimer) {
-        janusArgs.push(util.format(`--no-media-timer=${this._noMediaTimer}`));
-      }*/
-
-      let spawnOptions = {
-        detached: false,
-        /*
-         * fd 0 (stdin)   : Just ignore it.
-         * fd 1 (stdout)  : Pipe it for 3rd libraries in the worker.
-         *                  that log their own stuff.
-         * fd 2 (stderr)  : Same as stdout.
-         */
-        stdio: ["ignore", "pipe", "pipe"]
-        //    timeout: 3000,
-        //    killSignal: 'SIGKILL'
-      };
-
-      this._child = spawn(janusProcessname, janusArgs, spawnOptions);
-      console.log("janus: pid=" + this._child.pid);
-      this._child.stdout.on("data", buffer => {
-        if (buffer.slice(-1) == "\n") {
-          console.log(buffer.subarray(0, buffer.length - 1).toString());
-        } else {
-          console.log(buffer.toString());
-        }
-        if (buffer.toString().split("\n").some(o=>o===this.webSocketsThreadStarted)) {
-          resolve(this);
-        }
+      this.handler.push({
+        type: "error",
+        description: buffer.toString(),
+        status: true,
       });
 
-      this._child.stderr.on("data", buffer => {
-        if (buffer.slice(-1) == "\n") {
-          console.error(
-            "janus: stderr=" + buffer.subarray(0, buffer.length - 1).toString()
-          );
-        } else {
-          console.error("janus: stderr=" + buffer.toString());
-        }
-        // reject(buffer.toString());
-      });
+    });
 
-      this._child.on("exit", (code, signal) => {
-        console.log("janus: exit: code=" + code + " signal=" + signal);
-        this._child = null;
-        //this.close();
-        //this.emit("down");
-        reject({
-          code, signal
-        });
+    this._child.on("exit", (code, signal) => {
+      console.log("janus: exit: code=" + code + " signal=" + signal);
+      this._child = null;
+      //this.close();
+      //this.emit("down");
+      this.handler.push({
+        type: "terminated",
+        status: true
       });
+    });
 
-      this._child.on("error", error => {
-        console.error("janus: error=" + error);
-        this._child = null;
-        // this.close();
-        // this.emit("down");
-        // reject(error);
-      });
-    })
+    this._child.on("error", error => {
+      console.error("janus: error=" + error);
+      this._child = null;
+      // this.close();
+      // this.emit("down");
+    });
   }
 }
 module.exports = Janus;
--- controller/src/index.js
+++ controller/src/index.js
@@ -8,49 +8,55 @@ const JanusServer = require("./Janus");
 
 const initApp = async () => {
   console.log(">> init mediaServer", mediaServerEndPoints);
-  const janusInstance = await new JanusServer();
-  console.log(">> janusInstance instantiated");
-  console.log(">> init controller Node");
-  let node = await createNode();
-  console.log("node created");
-  console.log("node is ready", node.peerInfo.id.toB58String());
+  const janusInstance = new JanusServer();
+  pull(
+    janusInstance.handler,
+    pull.drain(async o=> {
+      console.log(o);
+      console.log(">> janusInstance instantiated");
+      console.log(">> init controller Node");
+      let node = await createNode();
+      console.log("node created");
+      console.log("node is ready", node.peerInfo.id.toB58String());
 
-  node.handle("/controller", (protocol, conn) => {
-    let sendToChannel = Pushable();
-    pull(sendToChannel, stringify(), conn);
-    pull(
-      conn,
-      pull.map(o => JSON.parse(o.toString())),
-      tap(console.log),
-      pull.drain(o => {})
-    );
-  });
-  node.on("peer:discovery", peerInfo => {
-    const idStr = peerInfo.id.toB58String();
-    // console.log("Discovered: " + idStr);
-    node.dialProtocol(peerInfo, "/streamer", (err, conn) => {
-      if (err) {
-        // console.error("Failed to dial:", err);
-        return;
-      }
-      let sendToStudio = Pushable();
-      pull(
-        sendToStudio,
-        stringify(),
-        conn,
-        pull.map(o => JSON.parse(o.toString())),
-        tap(console.log),
-        pull.log()
-      );
-    });
-  });
+      node.handle("/controller", (protocol, conn) => {
+        let sendToChannel = Pushable();
+        pull(sendToChannel, stringify(), conn);
+        pull(
+          conn,
+          pull.map(o => JSON.parse(o.toString())),
+          tap(console.log),
+          pull.drain(o => {})
+        );
+      });
+      node.on("peer:discovery", peerInfo => {
+        const idStr = peerInfo.id.toB58String();
+        // console.log("Discovered: " + idStr);
+        node.dialProtocol(peerInfo, "/streamer", (err, conn) => {
+          if (err) {
+            // console.error("Failed to dial:", err);
+            return;
+          }
+          let sendToStudio = Pushable();
+          pull(
+            sendToStudio,
+            stringify(),
+            conn,
+            pull.map(o => JSON.parse(o.toString())),
+            tap(console.log),
+            pull.log()
+          );
+        });
+      });
 
-  node.start(err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(node.peerInfo.multiaddrs.toArray().map(o => o.toString()));
-  });
+      node.start(err => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(node.peerInfo.multiaddrs.toArray().map(o => o.toString()));
+      });
+    })
+  );
 };
 initApp();
